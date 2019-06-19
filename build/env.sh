82db8a1d4933e8778dbf2ba25b1f7c9d9650d4a0
slavserver <onvosnovs@gmail.com>
2019-06-19 23:42:14 +0300

    Update env.sh

--- build/env.sh
+++ build/env.sh
@@ -11,10 +11,10 @@ fi
 workspace="$PWD/build/_workspace"
 root="$PWD"
 ethdir="$workspace/src/github.com/slavserver"
-if [ ! -L "$ethdir/open-ethereum-pool" ]; then
+if [ ! -L "$ethdir/pool" ]; then
     mkdir -p "$ethdir"
     cd "$ethdir"
-    ln -s ../../../../../. open-ethereum-pool
+    ln -s ../../../../../. pool
     cd "$root"
 fi
 
@@ -25,8 +25,8 @@ GOBIN="$PWD/build/bin"
 export GOPATH GOBIN
 
 # Run the command inside the workspace.
-cd "$ethdir/open-ethereum-pool"
-PWD="$ethdir/open-ethereum-pool"
+cd "$ethdir/pool"
+PWD="$ethdir/pool"
 
 # Launch the arguments with the configured environment.
 exec "$@"
